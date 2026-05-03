8f7b0320c460cb0222e11bc79fc49211867b685e
Manas Mayank <mayank.manas.2801@gmail.com>
2026-05-03 18:21:29 +0530

    remove complete_mission route

--- game/urls.py
+++ game/urls.py
@@ -2,16 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='root'),   # 👈 homepage = login
-
-    path('home/', views.home, name='home'),   # 👈 dashboard after login
+    path('', views.login_view, name='root'),
+    path('home/', views.home, name='home'),
 
     path('missions/', views.missions_view, name='missions'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
 
-    path('complete/<int:mission_id>/', views.complete_mission, name='complete_mission'),
     path('submit-proof/<int:mission_id>/', views.submit_proof, name='submit_proof'),
 
     path('leaderboard/', views.leaderboard_view, name='leaderboard'),
